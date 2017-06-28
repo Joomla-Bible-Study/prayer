71fdd084b5d186f67bd7ab9ddc662afbc41e20fb
bcordis <skeebrent@gmail.com>
2017-06-28 07:22:03 -0500

    Getting ready for PhPUnit Tests

---
+++ tests/unit/Com_CWMPrayerInstallerScriptTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package     ${NAMESPACE}
+ * @subpackage
+ *
+ * @copyright   A copyright
+ * @license     A "Slug" license name e.g. GPL2
+ */
+class Com_CWMPrayerInstallerScriptTest extends PHPUnit_Framework_TestCase
+{
+
+}
